227af5c23a832b5a46db80e936ae376e05bae677
Jonathan Augusto da Silva <jonathan.silva.992.js@gmail.com>
2025-08-26 20:06:22 +0000

    Coloque essas informacoes no app para poder fazer o login com firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyArQ_ubVkVG65jq2uWbf9CG0yX3PWyrMe4",
+  authDomain: "autoflow-fd4n5.firebaseapp.com",
+  projectId: "autoflow-fd4n5",
+  storageBucket: "autoflow-fd4n5.appspot.com",
+  messagingSenderId: "833766766449",
+  appId: "1:833766766449:web:9bcc0930bfbc01e274e8d0"
 };
 
 // Initialize Firebase
