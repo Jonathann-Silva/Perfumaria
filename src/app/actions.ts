e29a6a269a80ad9402454481725cbe5092ce8d26
Jonathan Augusto da Silva <jonathan.silva.992.js@gmail.com>
2026-01-09 17:44:20 +0000

    Try fixing this error: `Runtime AI_APICallError: Gone. Error source: src

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,19 +1,39 @@
 'use server';
 
-import { streamText } from 'ai';
-import { createOpenAI } from '@ai-sdk/openai';
+import { AIStream, StreamingTextResponse } from 'ai';
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
 
 export async function productChat(prompt: string) {
-    'use server';
-    const huggingface = createOpenAI({
-        baseURL: 'https://api-inference.huggingface.co/v1',
-        apiKey: process.env.HUGGINGFACE_API_KEY,
-    });
+    const huggingFaceResponse = await fetch(
+        'https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct',
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                inputs: prompt,
+                parameters: {
+                    max_new_tokens: 250,
+                    return_full_text: false,
+                },
+            }),
+        }
+    );
 
-    const { textStream } = await streamText({
-        model: huggingface('meta-llama/Meta-Llama-3-8B-Instruct'),
-        prompt,
-    });
+    // Check for errors
+    if (!huggingFaceResponse.ok) {
+        throw new Error(
+            `Hugging Face API request failed with status ${huggingFaceResponse.status}`
+        );
+    }
+    
+    // Convert the response into a ReadableStream
+    const stream = AIStream(huggingFaceResponse);
 
-    return textStream;
+    // Respond with the stream
+    return new StreamingTextResponse(stream).textStream;
 }
