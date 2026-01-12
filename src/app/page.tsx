c57dbfb5dd5adc081843230a5bdfd74197db59ec
Jonathann-Silva <168100734+Jonathann-Silva@users.noreply.github.com>
2026-01-11 22:26:10 -0300

    Trocando repositorio

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function RootPage() {
-  // A lógica de redirecionamento agora será tratada pelo AuthProvider
-  // Se não houver usuário, ele irá para /login.
-  // Se houver, irá para /dashboard.
-  redirect('/dashboard');
-}
